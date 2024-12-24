d5e3eb8bcfa7adc5a42db8e55bf97ed0d40d60c9
Olasunkanmi Oyinlola <olasunkanmi@regovtech.com>
2024-12-25 00:08:22 +0800

    fix build erroe

--- src/interfaces/generic.interface.ts
+++ src/interfaces/generic.interface.ts
@@ -1,3 +1,17 @@
+import { RequestHeader } from "../core/constants";
+
+export type HeadersType = {
+  [key in keyof RequestHeader]: string;
+};
+
+export interface IRequestOptions {
+  hostname: string;
+  path: string;
+  method: string;
+  headers?: any;
+  jwtToken?: string;
+}
+
 import ts from "typescript";
 
 export interface IFunctionInfo {
